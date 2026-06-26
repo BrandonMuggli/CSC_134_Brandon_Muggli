c4fa8d9f37ba82c4915c542e7d152b6141cc3249
BrandonMuggli <mugglibs704@mail.sandhills.edu>
2026-06-25 22:36:27 -0400

    Initial stages of Week 5 Labs 1-5

---
+++ Lab_5/rightTriangle.cpp
@@ -0,0 +1,15 @@
+/*
+@author: Brandon Muggli
+@date: 06/26/26
+@purpose: This program calculates the angle of a right triangle with specific parameters
+*/
+
+// include the iostream library for input/output and cmath for additional functions
+#include <iostream>
+#include <cmath>
+// use the standard namespace to avoid having to type std:: before every standard library function
+using namespace std;
+
+int main(){
+    
+}
